dbf2b8cea9a2c3d36421591a92ef49f955fec214
swheatley <shaylee1@me.com>
2015-06-25 20:21:26 -0600

    LRTHW Final Run throughstatus: PASSED, No ERRORS

--- ex26.rb
+++ ex26.rb
@@ -20,7 +20,7 @@ module Ex25
   # Prints the last word after popping it off.
   def Ex25.print_last_word(words)
     word = words.pop
-    put word
+    puts word
   end
 
   # Takes in a full sentence and returns the sorted words.
@@ -32,14 +32,14 @@ module Ex25
   # Prints the first and last words of the sentence.
   def Ex25.print_first_and_last(sentence)
     words = Ex25.break_words(sentence)
-    Ex25.print_first_word(word)
+    Ex25.print_first_word(words)
     Ex25.print_last_word(words)
   end
 
   # Sorts the words then prints the first and last one.
   def Ex25.print_first_and_last_sorted(sentence)
     words = Ex25.sort_sentence(sentence)
-    Ex25.print_fist_word(words)
+    Ex25.print_first_word(words)
     Ex25.print_last_word(words)
   end
 end
@@ -67,9 +67,9 @@ five = 10 - 2 + 3 - 6
 puts "This should be five: #{five}"
 
 def secret_formula(started)
-  jelly_bens = started * 500
+  jelly_beans = started * 500
   jars = jelly_beans / 1000
-  crate = jars / 100
+  crates = jars / 100
   return jelly_beans, jars, crates
 end
 
@@ -78,17 +78,17 @@ start_point = 10000
 beans, jars, crates = secret_formula(start_point)
 
 puts "With a starting point of: #{start_point}"
-puts "We\'d have #{beans}beans, #{jars} jars, and #{crates} crates."
+puts "We\'d have #{beans} beans, #{jars} jars, and #{crates} crates."
 
 start_point = start_point / 10
 
 sentence = "All good things come to those who wait."
 words = Ex25.break_words(sentence)
 sorted_words = Ex25.sort_words(words)
-Ex25.print_first_word(wrds)
-Ex25.print_last_word (words)
-Ex25.print_first_word(sort_words)
+Ex25.print_first_word(words)
+Ex25.print_last_word(words)
+Ex25.print_first_word(sorted_words)
 Ex25.print_last_word(sorted_words)
-sorted_words = Ex25.sort_sentenc(sentence)
+sorted_words = Ex25.sort_sentence(sentence)
 Ex25.print_first_and_last(sentence)
 Ex25.print_first_and_last_sorted(sentence)
