ce43f391673ff41505205db3e2efd233cd8c3fdc
swheatley <shaylee1@me.com>
2015-07-14 17:39:50 -0600

    Added explanations to the top of each code block.

--- ex30.rb
+++ ex30.rb
@@ -1,7 +1,10 @@
+# Three variables with values the represent people, cars and trucks
+
 people = 1
 cars = 45
 trucks = 5
 
+#If elsif loop. Cars are greater than people or trucks are less than cars.  
 if cars > people || trucks < cars
 	puts "We should take the cars."
 elsif cars < people
@@ -10,6 +13,7 @@ else
 	puts "We can't decide."
 end
 
+# If elsif loop.  Trucks are greater than cars and cars are less than people?
 if trucks > cars && cars < people
 	puts "That's too many trucks."
 elsif trucks < cars
@@ -17,7 +21,8 @@ elsif trucks < cars
 else
 	puts "We still can't decide"
 end
-
+#If else loop to see if there are more people than trucks.  
+#If there are more people then trucks we will take the trucks.
 if people > trucks
 	puts "Alright, let's just take the trucks."
 else 
